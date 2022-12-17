14da6a80259c45a5901f3274f74870202f98c30b
Brendon C Drew <brendoncdrew@gmail.com>
2022-12-17 05:19:18 -0800

    update to / /sdcard/ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -53,9 +53,9 @@ fi
 command+=" -b /dev"
 command+=" -b /proc"
 ## uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
+command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
-#command+=" -b /sdcard"
+command+=" -b /sdcard/ubuntu"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
